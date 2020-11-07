d5e0c0a53e8f7d7a91bf3cca528f772bec8b9c12
drprery <kpusztai777@gmail.com>
2020-11-07 18:29:07 +0100

    1. Project

---
+++ src/main/java/week02/Controller.java
@@ -0,0 +1,108 @@
+package week02;
+
+import java.util.Scanner;
+
+public class Controller {
+    private Office office;
+    private Scanner scanner = new Scanner(System.in);
+
+    public void readOffice() {
+        office = new Office();
+
+        System.out.print("A rögzítendő tárgyalók száma: ");
+        int mRooms = Integer.parseInt(scanner.nextLine());
+        System.out.println();
+
+        for (int i = 1; i <= mRooms; i++) {
+            System.out.print("A tárgyaló neve: ");
+            String mRoomName = scanner.nextLine();
+
+            System.out.print("A tárgyaló szélessége méterben: ");
+            int mRoomWidth = Integer.parseInt(scanner.nextLine());
+
+            System.out.print("A tárgyaló hosszúsága méterben: ");
+            int mRoomLength = Integer.parseInt(scanner.nextLine());
+            System.out.println();
+
+            office.addMeetingRoom(new MeetingRoom(mRoomName, mRoomWidth, mRoomLength));
+        }
+    }
+
+    public void printMenu() {
+        System.out.println("1. Tárgyalók sorrendben");
+        System.out.println("2. Tárgyalók visszafelé sorrendben");
+        System.out.println("3. Minden második tárgyaló");
+        System.out.println("4. Területek");
+        System.out.println("5. Keresés pontos név alapján");
+        System.out.println("6. Keresés névtöredék alapján");
+        System.out.println("7. Keresés terület alapján");
+        System.out.println("8. Kilépés a programból");
+        System.out.println();
+    }
+
+    public void runMenu() {
+        boolean quit = false;
+
+        while (!quit) {
+            System.out.print("A kért funkció száma:");
+            int funcNum = Integer.parseInt(scanner.nextLine());
+            switch (funcNum) {
+                case 1:
+                    office.printNames();
+                    System.out.println();
+                    printMenu();
+                    break;
+                case 2:
+                    office.printNamesReverse();
+                    System.out.println();
+                    printMenu();
+                    break;
+                case 3:
+                    office.printEvenNames();
+                    System.out.println();
+                    printMenu();
+                    break;
+                case 4:
+                    office.printAreas();
+                    System.out.println();
+                    printMenu();
+                    break;
+                case 5:
+                    System.out.print("A keresett tárgyaló neve:");
+                    String name = scanner.nextLine();
+                    System.out.println();
+                    office.printMeetingRoomsWithName(name);
+                    System.out.println();
+                    printMenu();
+                    break;
+                case 6:
+                    System.out.print("A keresett tárgyaló nevének töredéke: ");
+                    String pName = scanner.nextLine();
+                    System.out.println();
+                    office.printMeetingRoomsContains(pName);
+                    System.out.println();
+                    printMenu();
+                    break;
+                case 7:
+                    System.out.print("A keresett tárgyaló területe: ");
+                    int area = Integer.parseInt(scanner.nextLine());
+                    System.out.println();
+                    office.printAreasLargerThan(area);
+                    System.out.println();
+                    printMenu();
+                    break;
+                case 8:
+                    quit = true;
+            }
+        }
+        System.exit(1);
+    }
+
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+
+        controller.readOffice();
+        controller.printMenu();
+        controller.runMenu();
+    }
+}
---
+++ src/main/java/week02/Office.java
@@ -0,0 +1,93 @@
+package week02;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Office {
+    private List<MeetingRoom> meetingRooms = new ArrayList<>();
+
+    public void addMeetingRoom(MeetingRoom meetingRoom) {
+        meetingRooms.add(meetingRoom);
+    }
+
+    public void printNames() {
+        for (MeetingRoom mRoom : meetingRooms) {
+            System.out.println(mRoom.getName());
+        }
+    }
+
+    public void printNamesReverse() {
+        for (int i = meetingRooms.size() - 1; i >= 0; i--) {
+            System.out.println(meetingRooms.get(i).getName());
+        }
+    }
+
+    public void printEvenNames() {
+        for (int i = 1; i < meetingRooms.size(); i += 2) {
+            System.out.println(meetingRooms.get(i).getName());
+        }
+    }
+
+    public void printAreas() {
+        for (MeetingRoom mRoom : meetingRooms) {
+            System.out.println("A tárgyaló neve: " + mRoom.getName());
+            System.out.println("A tárgyaló szélessége: " + mRoom.getWidth());
+            System.out.println("A tárgyaló hosszúsága: " + mRoom.getLength());
+            System.out.println("A tárgyaló területe: " + mRoom.getArea());
+            System.out.println();
+        }
+    }
+
+    public void printMeetingRoomsWithName(String name) {
+        boolean found = false;
+        int index = 0;
+        while (!found && (index < meetingRooms.size())) {
+            MeetingRoom mRoom = meetingRooms.get(index);
+            index++;
+            if (mRoom.getName().equals(name)) {
+                System.out.println("A keresett tárgyaló szélessége: " + mRoom.getWidth());
+                System.out.println("A keresett tárgyaló hosszúsága: " + mRoom.getLength());
+                System.out.println("A keresett tárgyaló területe: " + mRoom.getArea());
+                found = true;
+            }
+        }
+        if(!found) {
+            System.out.println("Nincs ilyen nevű terem!");
+        }
+        System.out.println();
+    }
+
+    public void printMeetingRoomsContains(String part) {
+        boolean found = false;
+        int index = 0;
+        while (!found && (index < meetingRooms.size())) {
+            MeetingRoom mRoom = meetingRooms.get(index);
+            index++;
+            if ((mRoom.getName().toLowerCase().indexOf(part.toLowerCase())) >= 0) {
+                System.out.println("A keresett tárgyaló teljes neve: " + mRoom.getName());
+                System.out.println("A keresett tárgyaló szélessége: " + mRoom.getWidth());
+                System.out.println("A keresett tárgyaló hosszúsága: " + mRoom.getLength());
+                System.out.println("A keresett tárgyaló területe: " + mRoom.getArea());
+                found = true;
+            }
+        }
+        if(!found) {
+            System.out.println("Nincs ilyen nevű terem!");
+        }
+        System.out.println();
+    }
+
+    public void printAreasLargerThan(int area) {
+        for (MeetingRoom mRoom : meetingRooms) {
+            if(mRoom.getArea()>area){
+                System.out.println("A tárgyaló neve: " + mRoom.getName());
+                System.out.println("A tárgyaló szélessége: " + mRoom.getWidth());
+                System.out.println("A tárgyaló hosszúsága: " + mRoom.getLength());
+                System.out.println("A tárgyaló területe: " + mRoom.getArea());
+                System.out.println();
+            }
+        }
+    }
+
+}
