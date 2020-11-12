eb0dd7c506e909db27a9bb41c0799e6f9a0c312c
drprery <kpusztai777@gmail.com>
2020-11-12 21:22:22 +0100

    controlselection - Greetings class added

--- src/test/java/controlselection/greetings/GreetingsTest.java
+++ src/test/java/controlselection/greetings/GreetingsTest.java
@@ -8,14 +8,14 @@ public class GreetingsTest {
     @Test
     public void testGreet() {
         Greetings greetings = new Greetings();
-        assertEquals("jó éjt", greetings.greet(4, 30));
-        assertEquals("jó éjt", greetings.greet(5, 0));
-        assertEquals("jó reggelt", greetings.greet(5, 1));
-        assertEquals("jó reggelt", greetings.greet(9, 0));
-        assertEquals("jó napot", greetings.greet(9, 1));
-        assertEquals("jó napot", greetings.greet(18, 30));
-        assertEquals("jó estét", greetings.greet(18, 31));
-        assertEquals("jó estét", greetings.greet(20, 00));
-        assertEquals("jó éjt", greetings.greet(20, 01));
+        assertEquals("Jó éjt!", greetings.greet(4, 30));
+        assertEquals("Jó éjt!", greetings.greet(5, 0));
+        assertEquals("Jó reggelt!", greetings.greet(5, 1));
+        assertEquals("Jó reggelt!", greetings.greet(9, 0));
+        assertEquals("Jó napot!", greetings.greet(9, 1));
+        assertEquals("Jó napot!", greetings.greet(18, 30));
+        assertEquals("Jó estét!", greetings.greet(18, 31));
+        assertEquals("Jó estét!", greetings.greet(20, 00));
+        assertEquals("Jó éjt!", greetings.greet(20, 01));
     }
 }
