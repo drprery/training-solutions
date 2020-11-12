e1bb0160afbeeb04d146a96b893a6ae7f38e03ee
drprery <kpusztai777@gmail.com>
2020-11-12 17:00:26 +0100

    Subject class added

---
+++ src/main/java/records/Subject.java
@@ -0,0 +1,13 @@
+package records;
+
+public class Subject {
+    private String subjectName;
+
+    public Subject(String subjectname){
+        this.subjectName=subjectname;
+    }
+
+    public String getSubjectname() {
+        return subjectName;
+    }
+}
