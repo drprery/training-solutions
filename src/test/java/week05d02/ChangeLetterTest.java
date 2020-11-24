1e61498e8f9ff14881db85106c204206dc89233b
drprery <kpusztai777@gmail.com>
2020-11-24 10:37:35 +0100

    week05d02 - ChangeLetter

--- src/test/java/week05d02/ChangeLetterTest.java
+++ src/test/java/week05d02/ChangeLetterTest.java
@@ -1,7 +1,6 @@
 package week05d02;
 
 import org.junit.jupiter.api.Test;
-import stringscanner.StringScanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
