a3ca065d9d01023a4584dd38c00c5171ede06b9e
drprery <kpusztai777@gmail.com>
2020-11-25 17:56:43 +0100

    week05d02 - ChangeLetter v2.1, using StringBuilder

--- src/main/java/week05d02/ChangeLetter.java
+++ src/main/java/week05d02/ChangeLetter.java
@@ -4,20 +4,24 @@ public class ChangeLetter {
     public static final String CHARS="aeiou";
 
     public String changeVowels(String str){
-            String temp="";
+
+            String actualChar="";
+            StringBuilder sb=new StringBuilder("");
 
             if (str==null){
                 throw new IllegalArgumentException("Incorrect parameter string!");
             }
 
             for(int i=0;i<str.length();i++) {
-                if (CHARS.contains(str.substring(i, i + 1))) {
-                    temp = temp + "*";
+                actualChar=str.substring(i, i + 1);
+
+                if (CHARS.contains(actualChar)) {
+                    sb.append("*");
                 } else {
-                    temp = temp + str.substring(i, i + 1);
+                    sb.append(actualChar);
                 }
             }
 
-        return temp;
+        return sb.toString();
     }
 }
