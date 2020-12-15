2d44837781f2a0afd390f30fdacf132506ff456a
drprery <kpusztai777@gmail.com>
2020-12-15 21:46:10 +0100

    virtualmedthod.vehicle - Car, Van and Vehicle added

---
+++ src/main/java/virtualmethod/vehicle/Car.java
@@ -0,0 +1,26 @@
+package virtualmethod.vehicle;
+
+public class Car extends Vehicle{
+    private int numberOfPassenger;
+
+    public Car(int vehicleWeight, int numberOfPassenger) {
+        super(vehicleWeight);
+        this.numberOfPassenger = numberOfPassenger;
+    }
+
+    @Override
+    public int getGrossLoad() {
+        return super.getGrossLoad()+numberOfPassenger*Car.PERSON_AVERAGE_WEIGHT;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "numberOfPassenger=" + numberOfPassenger +
+                ", vehicleWeight="+super.getVehicleWeight()+"}";
+    }
+
+    public int getNumberOfPassenger() {
+        return numberOfPassenger;
+    }
+}
---
+++ src/main/java/virtualmethod/vehicle/Vehicle.java
@@ -0,0 +1,29 @@
+package virtualmethod.vehicle;
+
+public class Vehicle {
+    private int vehicleWeight;
+    public static final int PERSON_AVERAGE_WEIGHT=75;
+
+    public Vehicle(int vehicleWeight) {
+        this.vehicleWeight = vehicleWeight;
+    }
+
+    public int getGrossLoad(){
+        return PERSON_AVERAGE_WEIGHT+vehicleWeight;
+    }
+
+    public int getVehicleWeight() {
+        return vehicleWeight;
+    }
+
+    public static int getPersonAverageWeight() {
+        return PERSON_AVERAGE_WEIGHT;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "vehicleWeight=" + vehicleWeight +
+                '}';
+    }
+}
