7891302ae0df0fa381339cf9dd7dd9fcc3770975
drprery <kpusztai777@gmail.com>
2021-01-04 10:17:11 +0100

    week10d01 - Hiking

--- src/main/java/week10d01/Hiking.java
+++ src/main/java/week10d01/Hiking.java
@@ -3,17 +3,17 @@ package week10d01;
 import java.util.List;
 
 public class Hiking {
-    public double getPlusElevation(List<Gps> data){
-        double previous=data.get(0).getHeight();
-        double actual=0.0;
-        double elevationSum=0.0;
+    public double getPlusElevation(List<Gps> data) {
+        double previous = data.get(0).getHeight();
+        double actual = 0.0;
+        double elevationSum = 0.0;
 
-        for(int i=1;i< data.size();i++){
-            actual=data.get(i).getHeight();
-            if( actual>previous){
-                elevationSum+=(actual-previous);
+        for (int i = 1; i < data.size(); i++) {
+            actual = data.get(i).getHeight();
+            if (actual > previous) {
+                elevationSum += (actual - previous);
             }
-            previous=actual;
+            previous = actual;
         }
         return elevationSum;
     }
--- src/test/java/week10d01/HikingTest.java
+++ src/test/java/week10d01/HikingTest.java
@@ -9,8 +9,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HikingTest {
     @Test
-    public void testHiking(){
-        List<Gps> data=new ArrayList<>(List.of(new Gps(47.497913, 19.040236, 400.0), new Gps(47.497912, 19.040232, 420.0), new Gps(47.497916, 19.040236, 410.0)));
+    public void testHiking() {
+        List<Gps> data = new ArrayList<>(List.of(new Gps(47.497913, 19.040236, 400.0), new Gps(47.497912, 19.040232, 420.0), new Gps(47.497916, 19.040236, 410.0)));
 
         assertEquals(20, new Hiking().getPlusElevation(data));
     }
