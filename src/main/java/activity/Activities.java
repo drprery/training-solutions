21ae9525fdefbdaff6e2d4a2fdc1f18ca606d97e
drprery <kpusztai777@gmail.com>
2021-01-07 18:15:58 +0100

    activity - v1.0 beta

---
+++ src/main/java/activity/Activities.java
@@ -0,0 +1,50 @@
+package activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Activities {
+    private List<Activity> activities;
+
+    public Activities(List<Activity> activities) {
+        this.activities = activities;
+    }
+
+    public void addActivity(Activity activity){
+        activities.add(activity);
+    }
+
+    public List<Report> distancesByTypes(){
+        List<Report> report=new ArrayList<>();
+
+        for(Activity activity : activities){
+            report.add(new Report(activity.getType(), activity.getDistance()));
+        }
+
+        return report;
+    }
+
+    public int numberOfTrackActivities(){
+        int sumOfTrackActivies=0;
+
+        for(Activity activity : activities){
+            if(activity.getType()!= ActivityType.BASKETBALL){
+                sumOfTrackActivies++;
+            }
+        }
+
+        return sumOfTrackActivies;
+    }
+
+    public int numberOfWithoutTrackActivities(){
+        int sumOfWithoutTrackActivies=0;
+
+        for(Activity activity : activities){
+            if(activity.getType()== ActivityType.BASKETBALL){
+                sumOfWithoutTrackActivies++;
+            }
+        }
+
+        return sumOfWithoutTrackActivies;
+    }
+}
---
+++ src/main/java/activity/Track.java
@@ -0,0 +1,100 @@
+package activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Track {
+    private List<TrackPoint> trackPoints=new ArrayList<>();
+
+    public void addTrackPoint(TrackPoint trackPoint){
+        trackPoints.add(trackPoint);
+    }
+
+    public Coordinate findMaximumCoordinate(){
+        double latitude=Double.MIN_VALUE;
+        double longitude=Double.MIN_VALUE;
+
+        for(TrackPoint trackPoint : trackPoints){
+            if(trackPoint.getCoordinate().getLatitude()>latitude){
+                latitude=trackPoint.getCoordinate().getLatitude();
+            }
+
+            if(trackPoint.getCoordinate().getLongitude()>longitude){
+                longitude=trackPoint.getCoordinate().getLongitude();
+            }
+        }
+
+        return new Coordinate(latitude, longitude);
+    }
+
+    public Coordinate findMinimumCoordinate(){
+        double latitude=Double.MAX_VALUE;
+        double longitude=Double.MAX_VALUE;
+
+        for(TrackPoint trackPoint : trackPoints){
+            if(trackPoint.getCoordinate().getLatitude()<latitude){
+                latitude=trackPoint.getCoordinate().getLatitude();
+            }
+
+            if(trackPoint.getCoordinate().getLongitude()<longitude){
+                longitude=trackPoint.getCoordinate().getLongitude();
+            }
+        }
+
+        return new Coordinate(latitude, longitude);
+    }
+
+    public double getDistance(){
+        TrackPoint prevTrackPoint=trackPoints.get(0);
+        double distance=0.0;
+
+        for(int i=1;i<trackPoints.size();i++){
+            distance+=trackPoints.get(i).getDistanceFrom(prevTrackPoint);
+            prevTrackPoint=trackPoints.get(i);
+        }
+
+        return distance;
+    }
+
+    public double getFullDecrease(){
+        double decrease=0.0;
+        double prevAltitude=trackPoints.get(0).getElevation();
+
+        for(int i=1;i<trackPoints.size();i++){
+            if(prevAltitude>trackPoints.get(i).getElevation()){
+                decrease+=(prevAltitude-trackPoints.get(i).getElevation());
+            }
+            prevAltitude=trackPoints.get(i).getElevation();
+        }
+
+        return decrease;
+    }
+
+    public double getFullElevation(){
+        double elevation=0.0;
+        double prevElevation=trackPoints.get(0).getElevation();
+
+        for(int i=1;i<trackPoints.size();i++){
+            if(prevElevation<trackPoints.get(i).getElevation()){
+                elevation+=(trackPoints.get(i).getElevation()-prevElevation);
+            }
+            prevElevation=trackPoints.get(i).getElevation();
+        }
+
+        return elevation;
+    }
+
+    public double getRectangleArea(){
+        double latLength=0.0;
+        double longLength=0.0;
+
+        latLength=(Math.abs(findMinimumCoordinate().getLatitude())+Math.abs(findMaximumCoordinate().getLatitude()));
+        longLength=(Math.abs(findMinimumCoordinate().getLongitude())+Math.abs(findMaximumCoordinate().getLongitude()));
+
+        return latLength*longLength;
+    }
+
+    public List<TrackPoint> getTrackPoints() {
+        return trackPoints;
+    }
+}
