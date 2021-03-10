0008a22523a39e72342ab1f8c8869472bcc9facf
drprery <kpusztai777@gmail.com>
2021-03-10 12:11:14 +0100

    exam03retake02 - BalatonStorm

---
+++ src/main/java/exam03retake02/BalatonStorm.java
@@ -0,0 +1,43 @@
+package exam03retake02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+public class BalatonStorm {
+    public List<String> getStationsInStorm(BufferedReader reader){
+        String line;
+        String allomas;
+        int level;
+
+        List<String> stations = new ArrayList<>();
+
+        try{
+            while((line=reader.readLine())!=null){
+                if(line.contains("allomas")){
+                    allomas=line.substring(line.indexOf(": ")+3,line.length()-2);
+                    for(int i=1; i<=4; i++){
+                        line=reader.readLine();
+                    }
+                    level=Integer.parseInt(line.substring(line.indexOf(": ")+2,line.length()-1));
+                    if(level>=3){
+                        stations.add(allomas);
+                    }
+                }
+            }
+        } catch (IOException ioe){
+            throw new IllegalStateException();
+        }
+
+        Collator huCollator = Collator.getInstance(new Locale("HU","hu"));
+        huCollator.setStrength(Collator.PRIMARY);
+        Collections.sort(stations, huCollator);
+
+        return stations;
+    }
+}
